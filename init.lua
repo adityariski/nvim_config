688ec370b59bbc63f42a430fc8aad368a9075460
adityariski <adityariski0@gmail.com>
2024-12-07 09:36:36 +0700

    FIX

--- init.lua
+++ init.lua
@@ -902,6 +902,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
+  { "rest-nvim/rest.nvim" },
   {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
