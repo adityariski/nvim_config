7cb33b3de0bded230790e65fc5e14f9e5b2b2438
adityariski <adityariski0@gmail.com>
2024-12-21 00:58:27 +0700

    fix

--- init.lua
+++ init.lua
@@ -699,7 +699,9 @@ require('lazy').setup({
   -- {
   --   'uZer/pywal16.nvim',
   --   config = function()
-  --     vim.cmd.colorscheme 'pywal16'
+  --     -- vim.cmd.colorscheme 'pywal16'
+  --     local pywal16 = require('pywal16')
+  --     pywal16.setup()
   --   end,
   -- },
   {
